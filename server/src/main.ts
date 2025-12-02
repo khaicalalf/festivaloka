0d059942004512b78eed7bf5d771e5667ff5fdbe
Acmad Fadli Aditama <profadlibae@gmail.com>
2025-12-02 09:43:17 +0700

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -26,6 +26,7 @@ async function bootstrap() {
   // URL Swagger nanti di: http://localhost:3000/docs
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
